45706ed9acf893ff40556d6bcb8a2b4e6c4cc8ca
Vimal <vimalverma2004nov@gmail.com>
2023-06-17 00:42:42 +0530

    Update

---
+++ Test.cpp
@@ -0,0 +1,24 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <map>
+#include <set>
+#include <list>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#define ll long long
+using namespace std;
+ll solve()
+{
+}
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  ll t;cin>>t;
+  while(t--)
+  {
+     cout<<solve();
+     cout<<endl;
+  }
+}
