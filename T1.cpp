21dca99e5e34bd91916bff47749eaefaf512535a
Vimal <vimalverma2004nov@gmail.com>
2023-06-23 00:12:12 +0530

    upload

---
+++ T1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <list>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+int main()
+{
+  
+  cout<<endl;
+}
--- Test.cpp
+++ Test.cpp
@@ -1,14 +1,41 @@
-#include <iostream>
-#include <map>
-#include <set>
-#include <list>
-#include <vector>
-#include <algorithm>
-#include <cmath>
+#include "bits/stdc++.h"
+#define ll long long
 using namespace std;
- 
 int main()
 {
-  
-  cout<<endl;
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  ll t;cin>>t;
+  while(t--)
+  {
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (int i = 0; i < n;i++)
+      cin >> a[i];
+
+    int count = 0;
+    for (int i = 0; i < a.size(); i++)
+    {
+      int s = a[i];
+      vector<ll> temp;
+      int y = 0;
+      for (int j = i + 1; j < a.size(); j++)
+      {
+        if(s>=a[j])
+        {
+          s = s ^ a[j];
+          y = 1;
+        }
+        else
+          temp.push_back(a[j]);
+      }
+      if(y==1)
+        count++;
+      a = temp;
+    }
+    cout << count;
+    cout << endl;
+  }
 }
